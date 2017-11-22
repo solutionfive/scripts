46099d2915fd7d7379739181764485c784357305
solutionfive <citizenfive@lavabit.com>
2017-11-22 18:14:36 +0800

    Updated bbr_verify.sh

--- bbr_verify.sh
+++ bbr_verify.sh
@@ -6,10 +6,11 @@ SYSCTL_CONGESTION_RET=$(sysctl net.ipv4.tcp_congestion_control | awk -F'=' '{pri
 SYSCTL_QDISC_RET=$(sysctl net.core.default_qdisc | awk -F'=' '{print $2}')
 LSMOD_RET=$(lsmod | grep bbr  | awk -F' '  '{print $1}')
 
-if [ $KERNEL_VER == 4 ] && [ $SYSCTL_CONGESTION_RET == 'bbr' ] && [ $SYSCTL_QDISC_RET == 'bbr' ] && [ $LSMOD_RET == 'tcp_bbr' ]; then
-    echo -e 'BBR has been Verified.'
+
+if [ "$KERNEL_VER" == "4" ] && [ ${SYSCTL_CONGESTION_RET} == 'bbr' ] && [ ${SYSCTL_QDISC_RET} == 'fq' ] && [ ${LSMOD_RET} == 'tcp_bbr' ]; then
+    echo -e "BBR has been Verified."
 else
-    echo -e 'BBR failed with kernel_version: $KERNEL_VER, congestion: $SYSCTL_CONGESTION_RET, qdisc: $SYSCTL_QDISC_RET, lsmod: $LSMOD_RET'
+    echo -e "BBR failed with kernel_version: $KERNEL_VER, congestion: $SYSCTL_CONGESTION_RET, qdisc: $SYSCTL_QDISC_RET, lsmod: $LSMOD_RET"
 fi
 
 }
